b15f491beb3f725f4b3356562d5830c77a4956fe
Ansh Dani <152038786+AnshDani2004@users.noreply.github.com>
2024-11-16 12:41:08 -0700

    Initial Commit

---
+++ AeroTech/AeroTechApp.swift
@@ -0,0 +1,17 @@
+//
+//  AeroTechApp.swift
+//  AeroTech
+//
+//  Created by Ansh on 16/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AeroTechApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
